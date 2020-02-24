1ff19689d245961ba8e7ae2860c6522b815da252
remi-gai <56744348+remi-gai@users.noreply.github.com>
2020-02-24 14:12:11 -0800

    refactored proxy server redirect address to match with EC2

--- server/index.js
+++ server/index.js
@@ -17,18 +17,20 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('/gallery/:id', (req, res) => {
   const id = req.params.id;
 
-  axios.get(`http://localhost:3000/gallery/${id}`)
+  // axios.get(`http://localhost:3000/gallery/${id}`)
+  axios.get(`http://54.193.19.242:3000/gallery/${id}`)
     .then(({ data }) => {
       res.json(data);
     })
     .catch((error) => {
       console.log(error);
-    });
+    });s
 });
 
 // get gallery bundle
 app.get('/gallery', (req, res) => {
-  axios.get(`http://localhost:3000/bundle.js`)
+  // axios.get(`http://localhost:3000/bundle.js`)
+  axios.get(`http://54.193.19.242:3000/bundle.js`)
     .then(({ data }) => {
       res.send(data);
     })
